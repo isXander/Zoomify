e2ca5484eaeee541870c7478c19b65693c9dfe21
HarvelsX <90945793+HarvelsX@users.noreply.github.com>
2022-05-21 21:44:52 +0300

    Fix fov calculation with lagging or high fps (#29)
    Co-authored-by: isXander <isxander@users.noreply.github.com>

--- src/main/java/dev/isxander/zoomify/mixins/GameRendererMixin.java
+++ src/main/java/dev/isxander/zoomify/mixins/GameRendererMixin.java
@@ -2,15 +2,21 @@ package dev.isxander.zoomify.mixins;
 
 import dev.isxander.zoomify.Zoomify;
 import net.minecraft.client.MinecraftClient;
+import net.minecraft.client.render.Camera;
 import net.minecraft.client.render.GameRenderer;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.ModifyVariable;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+import org.spongepowered.asm.mixin.injection.callback.LocalCapture;
+
+import java.text.MessageFormat;
 
 @Mixin(GameRenderer.class)
 public class GameRendererMixin {
-    @ModifyVariable(method = "getFov", at = @At(value = "RETURN", shift = At.Shift.BEFORE), ordinal = 0)
-    private double getFov(double fov) {
-        return fov / Zoomify.getZoomDivisor(MinecraftClient.getInstance().getTickDelta());
+    @Inject(method = "getFov", at = @At(value = "RETURN", shift = At.Shift.BY), locals = LocalCapture.CAPTURE_FAILSOFT, cancellable = true)
+    private void getFov(Camera camera, float tickDelta, boolean changingFov, CallbackInfoReturnable<Double> cir) {
+        cir.setReturnValue(cir.getReturnValueD() / Zoomify.getZoomDivisor(tickDelta));
     }
 }
